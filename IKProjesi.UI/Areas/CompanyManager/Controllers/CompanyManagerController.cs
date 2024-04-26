cb47f3eb437c97aae549bc6349a9796951b645e8
ecek24 <eceekarakuss@gmail.com>
2024-04-26 17:04:24 +0300

    personel ekleme

---
+++ IKProjesi.UI/Areas/CompanyManager/Controllers/CompanyManagerController.cs
@@ -0,0 +1,36 @@
+﻿using IKProjesi.UI.Models.VMs.CompanyManagerVMs;
+using IKProjesi.UI.Models.VMs.PersonelVMs;
+using IKProjesi.UI.Services.Personel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKProjesi.UI.Areas.CompanyManager.Controllers
+{
+    public class CompanyManagerController : Controller
+    {
+        private readonly IPersonelApiService _personelApiService;
+
+        public CompanyManagerController(IPersonelApiService personelApiService)
+        {
+            _personelApiService = personelApiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePersonel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePersonel(CreatePersonelVM model)
+        {
+            await _personelApiService.CreatePersonel(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ IKProjesi.UI/Services/Personel/IPersonelApiService.cs
@@ -0,0 +1,12 @@
+﻿using IKProjesi.UI.Models.VMs.CompanyVMs;
+using IKProjesi.UI.Models.VMs.PersonelVMs;
+using Refit;
+
+namespace IKProjesi.UI.Services.Personel
+{
+    public interface IPersonelApiService
+    {
+        [Post("/api/CompanyManager/CreatePersonel")]
+        Task CreatePersonel(CreatePersonelVM model);
+    }
+}
