07bcaf13e44ce1f9d92b1cfd845b7d82e09b248c
DamlaGurel <damla.gureel@gmail.com>
2024-05-13 02:21:28 +0300

    view update

--- IKProjesi.UI/Models/Enums/Job.cs
+++ IKProjesi.UI/Models/Enums/Job.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
 namespace IKProjesi.UI.Models.Enums
 {
     public enum Job
     {
+        [Display(Name = "Süper Admin")]
         SUPERADMIN = 1,
+        [Display(Name = "Site Yöneticisi")]
         SITEMANAGER,
+        [Display(Name = "Şirket Yöneticisi")]
         COMPANYMANAGER,
+        [Display(Name = "Personel")]
         EMPLOYEE
     }
 }
