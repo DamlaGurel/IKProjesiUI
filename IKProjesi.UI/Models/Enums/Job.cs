a8d5e62894d221e325a520a5829a95548cd8b126
ecek24 <eceekarakuss@gmail.com>
2024-06-24 15:24:04 +0300

    job enum düzenlendi

--- IKProjesi.UI/Models/Enums/Job.cs
+++ IKProjesi.UI/Models/Enums/Job.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace IKProjesi.UI.Models.Enums
 {
     public enum Job
     {
+        [Display(Name = "Super Admin")]
         SUPERADMIN = 1,
+        [Display(Name = "Site Manager")]
         SITEMANAGER,
+        [Display(Name = "Company Manager")]
         COMPANYMANAGER,
+        [Display(Name = "Employee")]
         EMPLOYEE
     }
 }
